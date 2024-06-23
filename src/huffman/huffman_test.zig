943873a1bd278d911d27fe22f1a66017b17e284c
Marcus Harbol <mharbol@users.noreply.github.com>
2024-06-22 23:13:43 -0600

    bigger test for tree

--- src/huffman/huffman_test.zig
+++ src/huffman/huffman_test.zig
@@ -42,13 +42,45 @@ test "Test Combine two Leaves with Same Count" {
     const node0 = try huffman.HuffmanTreeNode.init(allocator, 12, 50);
     const node1 = try huffman.HuffmanTreeNode.init(allocator, 34, 50);
     const combined = try huffman.HuffmanTreeNode.combine(allocator, node0, node1);
-    defer {
-        combined.deinit(allocator);
-    }
+    defer combined.deinit(allocator);
     try std.testing.expect(combined.getByte() == 12);
     try std.testing.expect(combined.getCount() == 100);
     try std.testing.expect(combined.getLeft().?.getByte() == 12);
     try std.testing.expect(combined.getLeft().?.getCount() == 50);
     try std.testing.expect(combined.getRight().?.getByte() == 34);
     try std.testing.expect(combined.getRight().?.getCount() == 50);
+}
+
+test "Test Combine with 3" {
+    const node_c = try huffman.HuffmanTreeNode.init(allocator, 'C', 2);
+    const node_b = try huffman.HuffmanTreeNode.init(allocator, 'B', 6);
+    const node_e = try huffman.HuffmanTreeNode.init(allocator, 'E', 7);
+    const node_cb = try huffman.HuffmanTreeNode.combine(allocator, node_c, node_b);
+    var node_cbe = try huffman.HuffmanTreeNode.combine(allocator, node_e, node_cb);
+    defer node_cbe.deinit(allocator);
+
+    // root
+    try std.testing.expect(!node_cbe.isLeafNode());
+    try std.testing.expect(node_cbe.getByte() == 'B');
+    try std.testing.expect(node_cbe.getCount() == 15);
+
+    // left node off root (just E)
+    try std.testing.expect(node_cbe.getLeft().?.isLeafNode());
+    try std.testing.expect(node_cbe.getLeft().?.getByte() == 'E');
+    try std.testing.expect(node_cbe.getLeft().?.getCount() == 7);
+
+    // right node off root
+    try std.testing.expect(!node_cbe.getRight().?.isLeafNode());
+    try std.testing.expect(node_cbe.getRight().?.getByte() == 'B');
+    try std.testing.expect(node_cbe.getRight().?.getCount() == 8);
+
+    // left off right off root
+    try std.testing.expect(node_cbe.getRight().?.getLeft().?.isLeafNode());
+    try std.testing.expect(node_cbe.getRight().?.getLeft().?.getByte() == 'C');
+    try std.testing.expect(node_cbe.getRight().?.getLeft().?.getCount() == 2);
+
+    // right off right off root
+    try std.testing.expect(node_cbe.getRight().?.getRight().?.isLeafNode());
+    try std.testing.expect(node_cbe.getRight().?.getRight().?.getByte() == 'B');
+    try std.testing.expect(node_cbe.getRight().?.getRight().?.getCount() == 6);
 }
